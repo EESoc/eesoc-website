4798e78ff7c8ea7aafcd930057b19987313b9b76
jianyuan <jianyuan@gmail.com>
2013-10-06 08:35:32 +0100

    Force Instagram photo to server over HTTPS

--- app/models/InstagramPhoto.php
+++ app/models/InstagramPhoto.php
@@ -47,9 +47,9 @@ class InstagramPhoto extends Eloquent {
 		$photo->longitude                     = array_get($data, 'location.longitude');
 		$photo->captured_time                 = $data['created_time'];
 		$photo->link                          = $data['link'];
-		$photo->image_low_resolution_url      = array_get($data, 'images.low_resolution.url');
-		$photo->image_thumbnail_url           = array_get($data, 'images.thumbnail.url');
-		$photo->image_standard_resolution_url = array_get($data, 'images.standard_resolution.url');
+		$photo->image_low_resolution_url      = str_replace('http://', 'https://', array_get($data, 'images.low_resolution.url'));
+		$photo->image_thumbnail_url           = str_replace('http://', 'https://', array_get($data, 'images.thumbnail.url'));
+		$photo->image_standard_resolution_url = str_replace('http://', 'https://', array_get($data, 'images.standard_resolution.url'));
 		$photo->instagram_username            = array_get($data, 'user.username');
 		$photo->instagram_user_id             = array_get($data, 'user.id');
 		$photo->type                          = $data['type'];
