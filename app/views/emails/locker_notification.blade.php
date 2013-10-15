a6dd0311c59f2209f78bfb9d12958b101015fd32
Thomas Lim <txl11@ic.ac.uk>
2013-10-15 16:48:02 +0100

    Added terms and conds as separate file for existing owners, and in to the email they automatically receive upon purchase

--- app/views/emails/locker_notification.blade.php
+++ app/views/emails/locker_notification.blade.php
@@ -16,6 +16,12 @@
       <a href="https://eesoc.com/dashboard/lockers">https://eesoc.com/dashboard/lockers</a>
     </p>
     <p>
+      Please be aware that the lockers are not 100% secure and so should only be used for items of low value. The safety of the contents of lockers is your responsibility. Neither EESoc nor the Department accept any liability for any items lost or stolen.
+    </p>
+    <p>
+      Please be aware that combination padlocks are very insecure; a high percentage of thefts have been in lockers with these types of padlocks. All padlocks will only deter an opportunist thief; a committed thief will always break in.
+    </p>
+    <p>
       Kind Regards, <br/ >
       EESoc Locker Team
     </p>
