c5d09795bd30027109385a1659b3ae186e1c596d
jianyuan <jianyuan@gmail.com>
2013-10-25 16:59:35 +0100

    Add accordion to events page

--- app/views/events/index.blade.php
+++ app/views/events/index.blade.php
@@ -4,4 +4,22 @@
   <div class="page-header">
     <h1>Events</h1>
   </div>
+  <div class="panel-group" id="accordion-events">
+    @foreach ($events as $key => $event)
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h4 class="panel-title">
+            <a href="#event-{{ $event->id }}" class="accordion-toggle" data-toggle="collapse" data-parent="#accordion-events">
+              {{{ $event->name }}}
+            </a>
+          </h4>
+        </div>
+        <div id="event-{{ $event->id }}" class="panel-collapse collapse {{ $key === 0 ? 'in' : '' }}">
+          <div class="panel-body">
+            {{ $event->description }}
+          </div>
+        </div>
+      </div>
+    @endforeach
+  </div>
 @stop
