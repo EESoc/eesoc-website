d18343b037cc1dd5b50355a8c15ea235fa0663c1
jianyuan <jianyuan@gmail.com>
2013-11-18 16:24:21 +0000

    Add CSV

--- app/controllers/Admin/TestsController.php
+++ app/controllers/Admin/TestsController.php
@@ -3,6 +3,8 @@ namespace Admin;
 
 use \Auth;
 use \Notification;
+use \Response;
+use \User;
 
 class TestsController extends BaseController {
 
@@ -22,6 +24,43 @@ class TestsController extends BaseController {
 	{
 		Notification::sendLockerTermsAndConditions(Auth::user());
 		return 'OK';
+	}
+
+	public function getXmasNonAttendees()
+	{
+		function array2csv(array &$array)
+		{
+			if (count($array) == 0) {
+				return null;
+			}
+			ob_start();
+			$df = fopen("php://output", 'w');
+			fputcsv($df, array_keys(reset($array)));
+			foreach ($array as $row) {
+				fputcsv($df, $row);
+			}
+			fclose($df);
+			return ob_get_clean();
+		}
+
+		$users = User::whereNotIn('id', function($query) {
+			return $query->select('user_id')->from('christmas_dinner_sales');
+		})->get();
+
+		$csv_data = [];
+
+		foreach ($users as $user) {
+			$csv_data[] = [
+				'name' => $user->name,
+				'email' => $user->email,
+			];
+		}
+
+		$response = Response::make(array2csv($csv_data), 200);
+		$response->header('Content-Type', 'text/csv');
+		$response->header('Content-disposition', 'attachment;filename=xmas_non_attendees.csv');
+
+		return $response;
 	}
 
 }
