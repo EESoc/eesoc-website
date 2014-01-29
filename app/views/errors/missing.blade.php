bd680e82c03d8ab3a6c9a844ea9440ae6c5d58e3
jianyuan <jianyuan@gmail.com>
2014-01-29 17:17:49 +0000

    Update 404 page

--- app/views/errors/missing.blade.php
+++ app/views/errors/missing.blade.php
@@ -3,4 +3,6 @@
 @section('content')
   <h1>Oops! That page can't be found :(</h1>
   <p>Maybe something moved, or you mistyped.</p>
+  <p>We did find this though...</p>
+  <iframe width="560" height="315" src="//www.youtube.com/embed/JCPXckfT-6g?rel=0&amp;autoplay=1" frameborder="0" allowfullscreen></iframe>
 @stop
