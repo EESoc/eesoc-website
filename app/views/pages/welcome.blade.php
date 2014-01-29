017b2229b87ce83c50d06d2b44d20ef14712d172
jianyuan <jianyuan@gmail.com>
2014-01-29 18:15:06 +0000

    Fix header

--- app/views/pages/welcome.blade.php
+++ app/views/pages/welcome.blade.php
@@ -5,7 +5,6 @@
 @section('container')
 
 <div class="container">
-  <hr class="invisible">
   <div class="row">
     <div class="col-md-6">
       @content('homepage-content')
@@ -18,8 +17,10 @@
       </div>
     </div>
     <div class="col-md-6">
-      <a class="twitter-timeline"  href="https://twitter.com/EESoc"  data-widget-id="428571579799203840">Tweets by @EESoc</a>
-      <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+      <div style="margin-top: 20px;">
+        <a class="twitter-timeline"  href="https://twitter.com/EESoc"  data-widget-id="428571579799203840">Tweets by @EESoc</a>
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+      </div>
     </div>
   </div>
 </div>
