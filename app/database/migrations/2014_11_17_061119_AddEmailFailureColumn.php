1d8d5e69b204ca253ad15aa62dd526f264489925
Peter Bridgman <peter@xenon.bridgman.local>
2014-11-17 06:23:04 +0000

    Fix indentation with :retab

--- app/database/migrations/2014_11_17_061119_AddEmailFailureColumn.php
+++ app/database/migrations/2014_11_17_061119_AddEmailFailureColumn.php
@@ -4,26 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 
 class AddEmailFailureColumn extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
         Schema::table('newsletter_email_queue', function($table)
         {
             $table->boolean('failed')->default(FALSE);
         });
-	}
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
         $table->dropColumn('failed');
-	}
+    }
 }
