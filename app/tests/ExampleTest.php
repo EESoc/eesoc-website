c5cd8429c1bd1d1648b8589cd3dcb744bde81f17
Haaris <hsed@users.noreply.github.com>
2017-09-17 20:47:36 +0100

    Update ExampleTest.php

--- app/tests/ExampleTest.php
+++ app/tests/ExampleTest.php
@@ -9,7 +9,7 @@ class ExampleTest extends TestCase {
      */
     public function testBasicExample()
     {
-        $crawler = $this->client->request('GET', '/');
+        $this->client->request('GET', '/');
 
         $this->assertTrue($this->client->getResponse()->isOk());
     }
